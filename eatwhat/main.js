9ff40392f5ae169d6e1e87bbd1c5fc7938d57094
Yvonne <yvo.0802@gmail.com>
2021-04-23 17:42:11 +0800

    update eatwhat add meal

--- eatwhat/main.js
+++ eatwhat/main.js
@@ -1,21 +1,34 @@
+var topic = ["拉麵","滷肉飯","水餃"];
 //透過"$"來判斷是不是jQuery的語法
-$(function(){
+$(document).ready(function(){
+    $("button").click(function(){
+        var go=1;
+        if($(".meal").val()){
+        // if($(".num").val() && $(".num").val()<=topic.length+1)
+        topic.splice(topic.length,0,$(".meal").val());
+        $("ul").append(`<li>${topic[topic.length-1]}</li>`);
+        }
+    });
+   
+})
+$(function (){
     //找到input然後on(如果前面動作執行，坐後面的動作)
-    $("input").on("click",function(){
+    $("#input1").on("click",function(){
         
-        var numberOfListItem = $("li").length;
+        var numberOfListItem = topic.length;
         var randnum = Math.random()*numberOfListItem;
         var randomChildNumber = Math.floor(randnum);
-        $("h1").text($("li").eq(randomChildNumber).text());
-        
+        $("h1").text(topic[randomChildNumber]);
+        $("img").remove();
+        //$("ul").append('<img src=randomChildNumber+".jpg" alt=${topic[randomChildNumber]}>');
         if (randomChildNumber == 0){
-            $("img").remove();
+            
             $("ul").append('<img src="0.jpg" alt="拉麵">');
         }else if(randomChildNumber == 1){
-            $("img").remove();
+            
             $("ul").append('<img src="1.jpg" alt="滷肉飯">');
-        }else{
-            $("img").remove();
+        }else if(randomChildNumber == 2){
+
             $("ul").append('<img src="2.jpg" alt="水餃">');
         }
         //alert(randnum);
